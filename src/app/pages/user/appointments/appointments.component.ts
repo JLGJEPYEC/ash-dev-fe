4ba117b38e58362a30e3aa448acae073cde23afc
jolo-acer <juanlorenzo_800@hotmail.com>
2022-01-16 13:26:58 -0500

    añadir resultado de gestion citas

--- src/app/pages/user/appointments/appointments.component.ts
+++ src/app/pages/user/appointments/appointments.component.ts
@@ -8,6 +8,7 @@ import { MEDICS_DATA } from '../../../mock/medic.mock';
 import { APPOINTMENTS_DATA } from '../../../mock/appointment.mock';
 
 
+
 @Component({
   selector: 'app-appointments',
   templateUrl: './appointments.component.html',
@@ -29,6 +30,13 @@ export class AppointmentsComponent implements OnInit {
   specialties: Specialty[] = SPECIALTIES_DATA;
   medics!: Medic[];
   schedules: string[] = [];
+  //mostrar cita generada
+  selectedPerson!:string;
+  selectedMedic!: string;
+  selectedSpeciality!: string;
+  selectedDate!:string;
+  selectedSchedule!:string;
+
   constructor(
   ) {
     this.controlDni = new FormControl('',[Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/), Validators.minLength(8), Validators.maxLength(8)]),
@@ -52,6 +60,8 @@ export class AppointmentsComponent implements OnInit {
         this.id_history = ENSUREDS_DATA[i].id_history;
         this.enabled=true;
         console.log(this.id_history);
+        this.selectedPerson= this.name + " " + this.lastname;
+        //console.log(this.selectedPerson);
         return;
       }else{
         this.name = "";
@@ -61,8 +71,12 @@ export class AppointmentsComponent implements OnInit {
       }
     }
   }
+
   onSelectedSpecialty(){
     this.medics = MEDICS_DATA.filter(medic=> medic.id_specialty == this.controlSpecialty.value);
+    var mostrarEspecialidad : string = "";
+    mostrarEspecialidad = SPECIALTIES_DATA.filter(spec=> spec.id == this.controlSpecialty.value)[0].name; 
+    this.selectedSpeciality = "Especialidad es: "+mostrarEspecialidad;
   }
   onSelectedMedic(){
     this.schedules=[];
@@ -71,9 +85,19 @@ export class AppointmentsComponent implements OnInit {
         for(let j=0;j<MEDICS_DATA[i].schedules.length;j++){
           this.schedules.push(MEDICS_DATA[i].schedules[j].schedule);
         }
-        console.log(this.controlMedic.value)
+        this.selectedMedic = "Medico es: "+MEDICS_DATA[i].name+" "+MEDICS_DATA[i].lastname;
       }
     }
+  }
+
+  datosCitas() : string {
+    var form :string = "";
+    form = this.selectedPerson+"\n"+
+            this.selectedMedic+"\n"+
+            this.selectedSpeciality+"\n"+
+            this.selectedDate+"\n"+
+            this.selectedSchedule;
+    return form;
   }
 
   generateAppointment(){
@@ -86,7 +110,8 @@ export class AppointmentsComponent implements OnInit {
        this.controlSpecialty.valid &&
        !(this.controlSpecialty.value == "Seleccione") &&
        this.controlSchedule.valid &&
-       !(this.controlSchedule.value == "Seleccione")){
+       !(this.controlSchedule.value == "Seleccione"))
+       {
         APPOINTMENTS_DATA.push({
           id: id.concat("AP",(APPOINTMENTS_DATA.length+1).toString()),
           dni_ensured:this.dni,
@@ -95,7 +120,10 @@ export class AppointmentsComponent implements OnInit {
           appointment_date: this.controlDate.value,
           schedule:this.controlSchedule.value
         })
-        alert('Cita generada')
+        this.selectedDate = "fecha es: "+this.controlDate.value;
+        this.selectedSchedule = "horario es: "+this.schedules[0];
+        alert('Cita generada \n'+ this.datosCitas())
+        //resetear pantalla para que se vuelvan a registar citas
         this.dni=0;
         this.id_history="";
         this.name="";
@@ -105,10 +133,9 @@ export class AppointmentsComponent implements OnInit {
         this.controlMedic = new FormControl('',[Validators.required]),
         this.controlDate = new FormControl('',[Validators.required]),
         this.controlSchedule = new FormControl('',[Validators.required])
+        //fin del reseteo de pantalla
        }else{
          alert('Complete todos los campos')
        }
-    console.log(APPOINTMENTS_DATA.length);
-    console.log(this.controlDate.value)
   }
 }
