29741178167732b530c461c8d94cf833acaec420
jolo-acer <juanlorenzo_800@hotmail.com>
2022-01-17 18:52:29 -0500

    eliminacion de lo que no se entiende

--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -7,20 +7,23 @@ import { UserRoutingModule } from './user-routing.module';
 import { MaterialModuleModule } from 'src/app/angular-material/angular-material.module';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { AppointmentsComponent } from './appointments/appointments.component';
-import { AdmissionComponent } from './admission/admission.component';
-import { TopicalComponent } from './topical/topical.component';
+
 import { MatSlideToggleModule} from '@angular/material/slide-toggle';
-import { CardiologyComponent } from './cardiology/cardiology.component';
+
 import { MedicineComponent } from './medicine/medicine.component';
 import { HistoriesComponent } from './histories/histories.component';
-import { HospitalizationComponent } from './hospitalization/hospitalization.component';
-import { UrgencyComponent } from './urgency/urgency.component';
-import { EmergencyComponent } from './emergency/emergency.component';
-import { LaboratoryComponent } from './laboratory/laboratory.component';
+
 import { PatientsComponent } from './patients/patients.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
-  declarations: [UserComponent,AppointmentsComponent, AdmissionComponent, TopicalComponent, CardiologyComponent, MedicineComponent, HistoriesComponent, HospitalizationComponent, UrgencyComponent, EmergencyComponent, LaboratoryComponent, PatientsComponent, DashboardComponent],
+  declarations: [UserComponent,
+    AppointmentsComponent, 
+
+    MedicineComponent, 
+    HistoriesComponent, 
+
+    PatientsComponent, 
+    DashboardComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
